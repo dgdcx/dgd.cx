fe391157fcce2e01c1144bcfaa9c093dd86301b3
Sonicscream <Sonicscream@users.noreply.github.com>
2023-10-22 16:46:10 -0400

    Align text in the center

--- client/routes/chat.vue
+++ client/routes/chat.vue
@@ -8,8 +8,8 @@ export default {
 </script>
 
 <template>
-    <div><h1>Chat</h1></div>
     <main class="container p-0 d-flex flex-wrap flex-column align-items-center justify-content-center justify-content-lg-start">
+        <h1>Chat</h1>
         <Chat/>
     </main>
 </template>
