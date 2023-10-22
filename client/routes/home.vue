1cd58f3804c560ba36e8e7c24975d6459c6462b2
Sonicscream <Sonicscream@users.noreply.github.com>
2023-10-22 15:36:15 -0400

    Remove chat from home again

--- client/routes/home.vue
+++ client/routes/home.vue
@@ -1,11 +1,9 @@
 <script>
 import News from '../components/News.vue'
-import Chat from '../components/Chat.vue'
 import Carousel from '../components/Carousel.vue'
 export default {
     components: {
         News,
-        Chat,
         Carousel,
     }
 }
@@ -14,6 +12,6 @@ export default {
 <template>
     <div class="container p-0 mt-3 border rounded"><Carousel class="rounded overflow-hidden d-flex justify-content-center align-items-center shadow" style="height:20em;"/></div>
     <main class="container p-0 d-flex flex-wrap flex-column align-items-center justify-content-center justify-content-lg-start">
-        <News/><Chat/>
+        <News/>
     </main>
 </template>
