66a51acaa5d2a15e908b1338baa7ef4f4df6dd12
Sonicscream <Sonicscream@users.noreply.github.com>
2023-10-22 16:47:41 -0400

    add top margin

--- client/routes/chat.vue
+++ client/routes/chat.vue
@@ -8,8 +8,11 @@ export default {
 </script>
 
 <template>
-    <main class="container p-0 d-flex flex-wrap flex-column align-items-center justify-content-center justify-content-lg-start">
+    <main class="container p-0 mt-3 d-flex flex-wrap flex-column align-items-center justify-content-center justify-content-lg-start">
         <h1>Chat</h1>
         <Chat/>
     </main>
 </template>
+
+<style>
+</style>
