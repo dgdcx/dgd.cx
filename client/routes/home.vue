5d92fa7b0d69ea3a0ca1d92fced2540c51946732
Sonicscream <Sonicscream@users.noreply.github.com>
2023-10-22 15:32:55 -0400

    testing chat

--- client/routes/home.vue
+++ client/routes/home.vue
@@ -1,9 +1,11 @@
 <script>
 import News from '../components/News.vue'
+import Chat from '../components/Chat.vue'
 import Carousel from '../components/Carousel.vue'
 export default {
     components: {
         News,
+        Chat,
         Carousel,
     }
 }
@@ -12,6 +14,6 @@ export default {
 <template>
     <div class="container p-0 mt-3 border rounded"><Carousel class="rounded overflow-hidden d-flex justify-content-center align-items-center shadow" style="height:20em;"/></div>
     <main class="container p-0 d-flex flex-wrap flex-column align-items-center justify-content-center justify-content-lg-start">
-        <News/>
+        <News/><Chat/>
     </main>
 </template>
