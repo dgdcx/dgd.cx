906ee70db9bd9e3a9e9246f81505e79b280099a4
Sonicscream <Sonicscream@users.noreply.github.com>
2023-10-23 22:26:38 -0400

    Move stripe to separate module

--- server/index.mjs
+++ server/index.mjs
@@ -2,6 +2,7 @@ import 'dotenv/config'
 import express from 'express'
 import fs from 'fs'
 import { app } from './utility.mjs'
+import { router as stripe_router } from './stripe/stripe.mjs';
 
 import cors from 'cors'
 const corsOptions = {
@@ -15,56 +16,7 @@ app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-/*
-** STRIPE DONATION SYSTEM
-*/
-// Get and validate stripe settings from .env
-let getStripeSettings = () => {
-    let settings = {
-        valid: true,
-        key: process.env.STRIPE_PUBLISHABLE_KEY,
-        prices: {
-            donation_onetime_custom: process.env.STRIPE_PRICE_CUSTOM
-        },
-        success: "https://dgd.cx/stripe/success",
-        cancel: "https://dgd.cx/stripe/cancel"
-    }
-    if (typeof settings.key != typeof "" || settings.key.length < 1) {
-        settings.valid = false;
-        console.log(`settings.key seems to be invalid, disabling stripe checkout`);
-    }
-    for (let key in settings.prices) {
-        if (typeof settings.prices[key] != typeof "" || settings.prices[key].length < 1) {
-            settings.valid = false;
-            console.log(`settings.prices.${key} seems to be invalid, disabling stripe checkout`);
-        }
-        break;
-    }
-    return settings;
-};
-var stripeSettings = getStripeSettings();
-// Create a new stripe session
-app.post('/api/stripe/checkout', async (req, res) => {
-    if (!stripeSettings.valid) res.status(503).end();
-    const session = await stripe.checkout.sessions.create
-        ({
-            line_items
-                : [
-                    {
-                        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-                        price
-                            : process.env.STRIPE_PRICE_CUSTOM,
-                        quantity
-                            : 1,
-                    },
-                ],
-            mode: 'payment',
-            success_url: stripeSettings.success,
-            cancel_url: stripeSettings.cancel
-        });
-
-    res.redirect(303, session.url);
-});
+app.use('/api/stripe', stripe_router);
 
 /*
 ** DATA
---
+++ server/stripe/stripe.js
@@ -0,0 +1,48 @@
+import express from 'express';
+
+const router = express.Router();
+
+let getStripeSettings = () => {
+    let settings = {
+        valid: true,
+        key: process.env.STRIPE_PUBLISHABLE_KEY,
+        prices: {
+            donation_onetime_custom: process.env.STRIPE_PRICE_CUSTOM
+        },
+        success: "https://dgd.cx/stripe/success",
+        cancel: "https://dgd.cx/stripe/cancel"
+    }
+    if (typeof settings.key != typeof "" || settings.key.length < 1) {
+        settings.valid = false;
+        console.log(`settings.key seems to be invalid, disabling stripe checkout`);
+    }
+    for (let key in settings.prices) {
+        if (typeof settings.prices[key] != typeof "" || settings.prices[key].length < 1) {
+            settings.valid = false;
+            console.log(`settings.prices.${key} seems to be invalid, disabling stripe checkout`);
+        }
+        break;
+    }
+    return settings;
+};
+var stripeSettings = getStripeSettings();
+
+router.post('/checkout', async (req, res) => {
+    if (!stripeSettings.valid) res.status(503).end();
+    const session = await stripe.checkout.sessions.create
+        ({
+            line_items: [{
+                // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+                price
+                    : process.env.STRIPE_PRICE_CUSTOM,
+                quantity
+                    : 1,
+            }],
+            mode: 'payment',
+            success_url: stripeSettings.success,
+            cancel_url: stripeSettings.cancel
+        });
+    res.redirect(303, session.url);
+});
+
+export { router };
