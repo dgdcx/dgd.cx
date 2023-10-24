10a07131d065c93e08c41f624f5a7c72f1f4a13d
Sonicscream <Sonicscream@users.noreply.github.com>
2023-10-23 22:33:13 -0400

    fix

--- client/routes/donate.vue
+++ client/routes/donate.vue
@@ -2,27 +2,29 @@
     <div class="container my-3 d-flex flex-column justify-content-evenly align-items-center text-center"
         style="position:relative;">
         <h1>Donate</h1>
-        <div class="container d-flex flex-wrap justify-content-center align-items-center">
-            <form action="/api/stripe/checkout" method="POST">
+        <form action="/api/stripe/checkout" method="POST">
+            <div class="container d-flex flex-wrap justify-content-center align-items-center">
                 <div class="card">
-                    <img class="card-img-top" src="https://cdn.discordapp.com/app-assets/1159142823522746408/store/1162283505389146234.png?size=600">
+                    <img class="card-img-top"
+                        src="https://cdn.discordapp.com/app-assets/1159142823522746408/store/1162283505389146234.png?size=600">
                     <div class="card-body">
                         <h5 class="card-title">One-Time Donation</h5>
                         <p class="card-text">Select a custom amount to donate to The Dagda Federation.</p>
-                        <button type="submit" class="btn btn-success disabled">Coming Soon</button>
+                        <button type="submit" class="btn btn-success">Donate</button>
                     </div>
                 </div>
                 <div class="spacer d-none d-md-block"></div>
                 <div class="card">
-                    <img class="card-img-top" src="https://cdn.discordapp.com/app-assets/1159142823522746408/store/1162283505389146234.png?size=600">
+                    <img class="card-img-top"
+                        src="https://cdn.discordapp.com/app-assets/1159142823522746408/store/1162283505389146234.png?size=600">
                     <div class="card-body">
                         <h5 class="card-title">Recurring Donation</h5>
                         <p class="card-text">Select an amount to donate every month to The Dagda Federation.</p>
                         <button type="submit" class="btn btn-success disabled">Coming Soon</button>
                     </div>
                 </div>
-            </form>
-        </div>
+            </div>
+        </form>
         <img class="stripe-mark mt-sm-0 mt-4" src="/stripe/white.svg" />
     </div>
     <component is="script" src="https://js.stripe.com/v3/" async></component>
